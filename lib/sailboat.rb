d7c898ef9b5a243553b5d5a644e138d2e2f54aea
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 14:29:12 -0400

    fix: add leading zeros to 'hour' and 'minutes' attributes

--- lib/sailboat.rb
+++ lib/sailboat.rb
@@ -1,13 +1,14 @@
+require 'time'
+
 class Sailboat
   attr_reader :hour, :minutes, :meridian, :finish_day
 
   def initialize(completion_time)
     # require 'pry'; binding.pry
-    @hour = completion_time[0..1].to_i
-    @minutes = completion_time[3..4].to_i
+    @hour = format('%02d', completion_time[0..1]).to_i
+    @minutes = format('%02d', completion_time[3..4]).to_i
     @meridian = completion_time[6..7]
     @finish_day = completion_time[14..15].to_i
 
   end
-
 end
