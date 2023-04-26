39190770b453aa1a7679ec4b339b6b7497ac75c8
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 15:56:26 -0400

    test: add test to check that 'all_sailboats' attribute exists and count number equals 2

--- spec/contestants_spec.rb
+++ spec/contestants_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe Contestants do
 
     expect(contestants).to be_a Contestants
   end
+
+  it 'has attributes' do
+    contestants = Contestants.new(["12:00 PM, DAY 1", "02:01 PM, DAY 10"])
+
+    expect(contestants.all_sailboats.empty?).to eq(false)
+    expect(contestants.all_sailboats.count).to eq(2)
+  end
 end
