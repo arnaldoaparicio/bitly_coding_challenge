94cc2fddd900b29db467517c24c1c7557d1f10cb
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 18:42:51 -0400

    feat: add 'valid_contestants' method to attach to 'competitors' attribute

--- lib/sailboat_race.rb
+++ lib/sailboat_race.rb
@@ -1,11 +1,31 @@
 class SailboatRace
+  attr_reader :competitors, :starting_hour, :starting_minute, :starting_meridian, :max_completion_day
 
   def initialize(contestants, competition_time, max_completion_day)
-    @competitors = valid_contestants(contestants)
     @starting_hour = competition_time[0..1]
     @starting_minute = competition_time[3..4]
     @starting_meridian = competition_time[6..7]
     @max_completion_day = max_completion_day
+    @competitors = valid_contestants(contestants.all_sailboats)
+    # require 'pry'; binding.pry
   end
 
+  def valid_contestants(all_contestants)
+    array = []
+    all_contestants.each do |contestant|
+      # require 'pry'; binding.pry
+      if contestant.finish_day > @max_completion_day || contestant.finish_day == 0
+        p 'Invalid completion day'
+      elsif contestant.hour.to_i < 1 || contestant.hour.to_i > 12
+        p 'Invalid time'
+      elsif contestant.minutes.to_i < 0 || contestant.minutes.to_i > 59
+        p 'Invalid time'
+      else
+        array << contestant
+        p 'success'
+      end
+    end
+    array
+    require 'pry'; binding.pry
+  end
 end
