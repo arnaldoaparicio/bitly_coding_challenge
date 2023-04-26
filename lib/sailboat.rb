28c6041d9dc9e4cd79e72166ae8e28c18bb49275
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 18:38:41 -0400

    feature: add 'formatted_finish_day' method to attach to 'finish_day' attribute

--- lib/sailboat.rb
+++ lib/sailboat.rb
@@ -7,11 +7,18 @@ class Sailboat
     @hour = format('%02d', completion_time[0..1])
     @minutes = format('%02d', completion_time[3..4])
     @meridian = completion_time[6..7]
-    @finish_day = completion_time[14..15].to_i
+    @finish_day = formatted_finish_day(completion_time)
+    # @finish_day = completion_time[14..15].to_i
 
   end
 
   def formatted_time
     Time.parse("#{hour}:#{minutes} #{meridian}").strftime("%I:%M %p")
+  end
+
+  def formatted_finish_day(completion_time)
+    return completion_time[14..15].to_i if completion_time.length == 15
+    return completion_time[14..15].to_i if completion_time.length == 16
+    return completion_time[14..16].to_i if completion_time.length == 17
   end
 end
