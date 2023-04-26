dfb15b3e5313a98ea02edc783cf5b72e67f68b70
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 14:52:21 -0400

    fix: remove 'to_i' from 'hour' and 'minutes' attributes

--- lib/sailboat.rb
+++ lib/sailboat.rb
@@ -5,8 +5,8 @@ class Sailboat
 
   def initialize(completion_time)
     # require 'pry'; binding.pry
-    @hour = format('%02d', completion_time[0..1]).to_i
-    @minutes = format('%02d', completion_time[3..4]).to_i
+    @hour = format('%02d', completion_time[0..1])
+    @minutes = format('%02d', completion_time[3..4])
     @meridian = completion_time[6..7]
     @finish_day = completion_time[14..15].to_i
 
