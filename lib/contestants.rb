8203429526ca12c4f30f446be2caaab509bd9404
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 15:55:47 -0400

    feat: add 'format_contestants' method that allow array of strings to create an instance of Sailboat

--- lib/contestants.rb
+++ lib/contestants.rb
@@ -1,6 +1,13 @@
 class Contestants
+  attr_reader :all_sailboats
 
   def initialize(all_contestants)
     @all_sailboats = format_contestants(all_contestants)
+  end
+
+  def format_contestants(all_contestants)
+    all_contestants.map do |contestant|
+      Sailboat.new(contestant)
+    end
   end
 end
