91df07d021032c9ca6d86b2067e09de21e1a1742
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 15:21:52 -0400

    feat: add formatted_time method

--- lib/sailboat.rb
+++ lib/sailboat.rb
@@ -4,11 +4,14 @@ class Sailboat
   attr_reader :hour, :minutes, :meridian, :finish_day
 
   def initialize(completion_time)
-    # require 'pry'; binding.pry
     @hour = format('%02d', completion_time[0..1])
     @minutes = format('%02d', completion_time[3..4])
     @meridian = completion_time[6..7]
     @finish_day = completion_time[14..15].to_i
 
+  end
+
+  def formatted_time
+    Time.parse("#{hour}:#{minutes} #{meridian}").strftime("%I:%M %p")
   end
 end
