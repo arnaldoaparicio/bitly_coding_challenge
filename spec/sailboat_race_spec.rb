27a0f8cb8a960a4370c7e28d1db379eb6c1dbb8f
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-26 16:27:24 -0400

    test: add tests to check that attributes exist

--- spec/sailboat_race_spec.rb
+++ spec/sailboat_race_spec.rb
@@ -5,8 +5,19 @@ require './lib/sailboat_race'
 RSpec.describe SailboatRace do
   it 'exists' do
     contestants = Contestants.new(["02:00 PM, DAY 19", "02:00 PM, DAY 20", "01:58 PM, DAY 20"])
-    race = SailboatRace.new(contestants)
+    race = SailboatRace.new(contestants, "08:00 AM, DAY 1", 99)
 
     expect(race).to be_a SailboatRace
+  end
+
+  it 'has attributes' do
+    contestants = Contestants.new(["02:00 PM, DAY 19", "02:00 PM, DAY 20", "01:58 PM, DAY 20"])
+    race = SailboatRace.new(contestants, "08:00 AM, DAY 1", 99)
+
+    expect(race.competitors.count).to eq(3)
+    expect(race.starting_hour).to eq('08')
+    expect(race.starting_minute).to eq('00')
+    expect(race.starting_day).to eq(1)
+    expect(race.max_completion_day).to eq(99)
   end
 end
