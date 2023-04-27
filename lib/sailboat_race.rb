57f0b1ee16ed3b1426d313f59154ee11270f1365
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 07:27:59 -0400

    feat: add contestant_average_minutes' check the race average for all contestants

--- lib/sailboat_race.rb
+++ lib/sailboat_race.rb
@@ -13,18 +13,26 @@ class SailboatRace
     array = []
     all_contestants.each do |contestant|
       if contestant.finish_day > @max_completion_day || contestant.finish_day == 0
-        p 'Invalid completion day'
+        nil
       elsif contestant.hour.to_i < 1 || contestant.hour.to_i > 12
-        p 'Invalid time'
+        nil
       elsif !contestant.meridian == 'PM' || !contestant.meridian == 'AM'
-        p 'Invalid meridian'
+        nil
       elsif contestant.minutes.to_i < 0 || contestant.minutes.to_i > 59
-        p 'Invalid time'
+        nil
       else
         array << contestant
-        p 'success'
+        # puts 'success'
       end
     end
     array
+  end
+
+  def contestant_average_minutes
+    total_minutes = 0
+    @competitors.each do |competitor|
+      total_minutes += competitor.minutes_lapsed
+    end
+    (total_minutes.to_f / competitors.length).round
   end
 end
