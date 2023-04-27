dad5d314de43a685a250966c1e6c3830405801db
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 07:26:04 -0400

    feat: add 'minutes_lapsed' method to check the number of minutes it takes for a single competitor to complete a race

--- lib/sailboat.rb
+++ lib/sailboat.rb
@@ -1,15 +1,14 @@
 require 'time'
 
 class Sailboat
-  attr_reader :hour, :minutes, :meridian, :finish_day
+  attr_reader :hour, :minutes, :meridian, :finish_day, :start_day
 
   def initialize(completion_time)
     @hour = format('%02d', completion_time[0..1])
     @minutes = format('%02d', completion_time[3..4])
     @meridian = completion_time[6..7]
     @finish_day = formatted_finish_day(completion_time)
-    # @finish_day = completion_time[14..15].to_i
-
+    @start_day = 1
   end
 
   def formatted_time
@@ -20,5 +19,25 @@ class Sailboat
     return completion_time[14..15].to_i if completion_time.length == 15
     return completion_time[14..15].to_i if completion_time.length == 16
     return completion_time[14..16].to_i if completion_time.length == 17
+  end
+
+  def minutes_lapsed
+    a = Time.parse("08:00 AM")
+    minute = 60
+    total_minute = 0
+    day = a.day
+    
+    loop do 
+      a = a + minute
+      total_minute += 1
+      current_day = a.day
+      if current_day - day == 1
+        @start_day += 1
+      end
+     if @start_day == @finish_day && formatted_time == Time.parse(a.to_s).strftime("%I:%M %p")
+      break
+     end
+    end
+    total_minute
   end
 end
