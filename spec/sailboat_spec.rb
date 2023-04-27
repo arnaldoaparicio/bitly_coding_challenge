2d89520ba71018a83db97638898bf94e95e8afe3
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 17:16:43 -0400

    delete: remove test for deleted 'formatted_time' method

--- spec/sailboat_spec.rb
+++ spec/sailboat_spec.rb
@@ -3,13 +3,13 @@ require 'time'
 
 RSpec.describe Sailboat do
   it 'exists' do
-    sailboat = Sailboat.new("12:00 PM, DAY 1")
+    sailboat = Sailboat.new('12:00 PM, DAY 1')
 
     expect(sailboat).to be_a Sailboat
   end
 
   it 'has attributes' do
-    sailboat = Sailboat.new("12:00 PM, DAY 1")
+    sailboat = Sailboat.new('12:00 PM, DAY 1')
     
     expect(sailboat.hour).to eq('12')
     expect(sailboat.minutes).to eq('00')
@@ -18,7 +18,7 @@ RSpec.describe Sailboat do
   end
 
   it 'lists day number 15' do
-    sailboat = Sailboat.new("12:00 PM, DAY 15")
+    sailboat = Sailboat.new('12:00 PM, DAY 15')
 
     expect(sailboat.finish_day).to eq(15)
   end
@@ -32,14 +32,8 @@ RSpec.describe Sailboat do
     expect(sailboat.finish_day).to eq(99)
   end
 
-  it 'has the formatted time' do
-    sailboat = Sailboat.new('03:03 AM, DAY 99')
-    
-    expect(sailboat.formatted_time).to eq('03:03 AM')
-  end
-
   it 'counts the number of minutes for one competitor within a day' do
-  sailboat = Sailboat.new("12:00 PM, DAY 1")
+    sailboat = Sailboat.new('12:00 PM, DAY 1')
     expect(sailboat.minutes_lapsed).to eq(240)
   end
 end
