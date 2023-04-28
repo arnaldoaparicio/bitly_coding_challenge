2f61df40a095712eef40003f1a9735fcfdb28522
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 20:35:31 -0400

    fix: add comments for clarity

--- lib/sailboat.rb
+++ lib/sailboat.rb
@@ -1,6 +1,7 @@
 require 'date'
 require 'time'
 
+# Sailboat class with all time information
 class Sailboat
   attr_reader :hour, :minutes, :meridian, :finish_day, :start_day
 
@@ -13,18 +14,21 @@ class Sailboat
     @total_time = 0
   end
 
+# Formats the finish hour to add leading zeros when necessary for single digit numbers
   def format_hour(completion_time)
     return format('%02d', completion_time[1]) if completion_time[0..1].to_i.digits.length == 1
 
     format('%02d', completion_time[0..1])
   end
 
+  # Formats Finish day based on String length
   def formatted_finish_day(completion_time)
     return completion_time[14..15].to_i if completion_time.length == 15
     return completion_time[14..15].to_i if completion_time.length == 16
     return completion_time[14..16].to_i if completion_time.length == 17
   end
 
+# Finds the number of minutes taken to complete race for self
   def minutes_lapsed
     if @finish_day > 1
       days_lapsed_in_minutes = ((@finish_day.to_i - 1) * 1440) - 480
