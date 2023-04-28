ebf709449d8aec5b4ae1becbed0b85ad076830c3
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 22:37:55 -0400

    doc: add comment to clarify how it works

--- lib/average_minutes.rb
+++ lib/average_minutes.rb
@@ -2,6 +2,7 @@ require './lib/sailboat_race'
 require './lib/sailboat'
 require './lib/contestants'
 
+# Array of times is passed through using user-input.
 begin
   contestants = Contestants.new(ARGV)
   race = SailboatRace.new(contestants)
