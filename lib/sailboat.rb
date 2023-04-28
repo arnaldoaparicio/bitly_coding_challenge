7b0f7a85a97047ac7dbbbe2331815b54556b06f6
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 22:44:46 -0400

    fix: replace all instances of 'meridian' to 'meridiem'

--- lib/sailboat.rb
+++ lib/sailboat.rb
@@ -3,12 +3,12 @@ require 'time'
 
 # Sailboat class with all time information
 class Sailboat
-  attr_reader :hour, :minutes, :meridian, :finish_day, :start_day
+  attr_reader :hour, :minutes, :meridiem, :finish_day, :start_day
 
   def initialize(completion_time)
     @hour = format_hour(completion_time)
     @minutes = format('%02d', completion_time[3..4])
-    @meridian = completion_time[6..7]
+    @meridiem = completion_time[6..7]
     @finish_day = formatted_finish_day(completion_time)
     @start_day = 1
     @total_time = 0
@@ -32,13 +32,13 @@ class Sailboat
   def minutes_lapsed
     if @finish_day > 1
       days_lapsed_in_minutes = ((@finish_day.to_i - 1) * 1440) - 480
-      remainder_time = Time.strptime("#{hour}:#{minutes} #{meridian}", '%I:%M %P').strftime('%H:%M').to_i * 60
+      remainder_time = Time.strptime("#{hour}:#{minutes} #{meridiem}", '%I:%M %P').strftime('%H:%M').to_i * 60
       @total_time = days_lapsed_in_minutes + remainder_time + @minutes.to_i
 
       @total_time
 
     elsif @finish_day == 1
-      hours_lapsed_in_minutes = Time.strptime("#{hour}:#{minutes} #{meridian}", '%I:%M %P').strftime('%H:%M').to_i * 60
+      hours_lapsed_in_minutes = Time.strptime("#{hour}:#{minutes} #{meridiem}", '%I:%M %P').strftime('%H:%M').to_i * 60
 
       (hours_lapsed_in_minutes + @minutes.to_i) - 480
 
