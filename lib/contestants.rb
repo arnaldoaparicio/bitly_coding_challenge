ab62ef2d0423d84ea8067adf634eab4cd3446014
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 20:35:00 -0400

    fix: add comments for clarity

--- lib/contestants.rb
+++ lib/contestants.rb
@@ -1,10 +1,12 @@
 class Contestants
+# Contestants class capable of converting each time to Sailboat instance
   attr_reader :all_sailboats
 
   def initialize(all_contestants)
     @all_sailboats = format_contestants(all_contestants)
   end
 
+# Converts each time to Sailboat instance
   def format_contestants(all_contestants)
     all_contestants.map do |contestant|
       Sailboat.new(contestant)
