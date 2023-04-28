c681b91fdb14e22c564ff8124fd9db459d5eb895
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 20:35:46 -0400

    fix: add comments for clarity

--- lib/sailboat_race.rb
+++ lib/sailboat_race.rb
@@ -1,6 +1,8 @@
 class SailboatRace
   attr_reader :competitors, :starting_hour, :starting_minute, :starting_meridian, :max_completion_day
 
+# SailboatRace class with valid competitors, start time/day information, and last race day
+# competition_time and max_completion_day have default values but can be overridden
   def initialize(contestants, competition_time = '08:00 AM, DAY 1', max_completion_day = 99)
     @starting_hour = competition_time[0..1]
     @starting_minute = competition_time[3..4]
@@ -9,6 +11,7 @@ class SailboatRace
     @competitors = valid_contestants(contestants.all_sailboats)
   end
 
+# Checks that contestants' times are valid and in proper format
   def valid_contestants(all_contestants)
     valid_array = []
     all_contestants.each do |contestant|
@@ -27,6 +30,7 @@ class SailboatRace
     valid_array
   end
 
+# Finds the average minutes of each contestant for race
   def contestant_average_minutes
     total_minutes = 0
     @competitors.each do |competitor|
