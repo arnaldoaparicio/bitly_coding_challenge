535e8b07a512b31d68b7ea61fd6d17c5509d4274
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-27 21:57:42 -0400

    feat: add basic error handling

--- lib/average_minutes.rb
+++ lib/average_minutes.rb
@@ -2,7 +2,12 @@ require './lib/sailboat_race'
 require './lib/sailboat'
 require './lib/contestants'
 
-contestants = Contestants.new(["02:00 PM, DAY 19", "02:00 PM, DAY 20", "01:58 PM, DAY 20"])
-race = SailboatRace.new(contestants)
+begin
+  contestants = Contestants.new(ARGV)
+  race = SailboatRace.new(contestants)
 
-puts race.contestant_average_minutes
+  puts race.contestant_average_minutes
+
+rescue => e
+  puts 'Uh Oh! It broke somewhere!'
+end
